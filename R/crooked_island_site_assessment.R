8a3be6292db185e7fa7005d56a99335a239fd2f0
ifoxfoot <ifoxfoot@gmail.com>
2022-07-13 13:57:40 -0700

    Added FQI function, plus help documentation

---
+++ R/crooked_island_site_assessment.R
@@ -0,0 +1,14 @@
+#' A List of Plants from Crooked Island, MI
+#'
+#' A dataset containing a list of plants from a site assessment on crooked island in open dunes,
+#' in Michigan.
+#'
+#' @format A data frame with 53940 rows and 3 variables:
+#' \describe{
+#'   \item{scientific_name}{scientific name}
+#'   \item{acronym}{a unique acronym for each plant}
+#'   \item{common_name}{common names for said plant}
+#'   ...
+#' }
+#' @source \url{https://universalfqa.org/}
+"crooked_island_site_assessment"
--- R/total_mean_c.R
+++ R/total_mean_c.R
@@ -1,6 +1,3 @@
-
-#this is the function that calculates total mean c values
-
 #' Calculate Mean C
 #'
 #' @param assessment A dataframe containing a list of plant species. This dataframe must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
@@ -25,3 +22,28 @@ total_mean_c <- function(assessment) {
   print(mean_c)
 
   }
+
+
+#' Calculate Total FQI
+#'
+#' @param assessment A dataframe containing a list of plant species. This dataframe must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#'
+#' @return A value for total FQI
+#' @export
+#'
+#' @examples
+#' plant_list <- crooked_island_site_assessment
+#' total_mean_c(assessment = plant_list)
+
+total_FQI <- function(assessment) {
+
+  #join scores from michigan fqai to user's assessment
+  user_list_with_scores <- dplyr::left_join(assessment, michigan_2014_fqai)
+
+  #calculate mean C
+  fqi <- mean(user_list_with_scores$c) * sqrt(nrow(assessment))
+
+  #print
+  print(fqi)
+
+}
