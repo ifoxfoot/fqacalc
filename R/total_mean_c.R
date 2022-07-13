f97532130711314d0e73cffc41e130a3a4d10ad2
ifoxfoot <ifoxfoot@gmail.com>
2022-07-13 13:12:58 -0700

    added liscense, edited description

--- R/total_mean_c.R
+++ R/total_mean_c.R
@@ -1,6 +1,18 @@
 
 #this is the function that calculates total mean c values
 
+#' Calculate Mean C
+#'
+#' @param assessment A dataframe containing a list of plant species. This dataframe must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#'
+#'
+#' @return A value for mean C
+#' @export
+#'
+#' @examples
+#' plant_list <- crooked_island_site_assessment
+#' total_mean_c(assessment = plant_list)
+
 total_mean_c <- function(assessment) {
 
   #join scores from michigan fqai to user's assessment
