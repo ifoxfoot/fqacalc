742823346cad38c56ff4554c8f049f884f17d552
ifoxfoot <ifoxfoot@gmail.com>
2022-07-15 11:18:55 -0700

    changed species richness functions to represent unique entries instead of total entries. Updated help files.

--- R/total_mean_c.R
+++ R/total_mean_c.R
@@ -1,7 +1,16 @@
 
+#this prevents a note about native being an undefined global variable
+utils::globalVariables("native")
+
+#-------------------------------------------------------------------------------
+
 #' Calculate Number of Species
 #'
-#' @param x A data frame containing a list of plant species. This data frame must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#' `total_species_richness()` calculates the total number of species in the site
+#' assessment.
+#'
+#' @param x A data frame containing a list of plant species. This data frame must
+#' have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
 #'
 #'
 #' @return A non-negative integer
@@ -13,8 +22,8 @@
 
 total_species_richness <- function(x) {
 
-  #count how many observations are in species list
-  species_richness <- nrow(x)
+  #count how many unique observations are in species list
+  species_richness <- nrow(unique(x))
 
   #return number of species
   return(species_richness)
@@ -25,7 +34,12 @@ total_species_richness <- function(x) {
 
 #' Calculate Number of Native Species
 #'
-#' @param x A data frame containing a list of plant species. This data frame must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#' `native_species_richness()` calculates the total number of native species in the
+#' site assessment.
+#'
+#' @param x A data frame containing a list of plant species. This data frame
+#' must have one of the following columns: `scientific_name`, `acronym`, or
+#' `common_name`.
 #'
 #' @return A non-negative integer
 #' @export
@@ -37,16 +51,22 @@ total_species_richness <- function(x) {
 native_species_richness <- function(x) {
 
   #join scores from Michigan FQAI to user's assessment
-  user_list_with_scores <- dplyr::left_join(x, michigan_2014_fqai)
+  user_list_with_scores <- suppressMessages(
+    dplyr::left_join(x, michigan_2014_fqai))
+
+  #send warning to user if site assessment contains plant not in FQAI database
+  if( any(is.na(user_list_with_scores$c)) )
+    message("Species not listed in database, it will be treated as a non-native")
 
   #select native plants
   native_species <- user_list_with_scores %>%
-    dplyr::filter(native == "native") %>%
-    #count observations
-    nrow()
+    dplyr::filter(native == "native")
+
+  #count unique observations
+  native_num <- nrow(unique(native_species))
 
   #return number of native species
-  return(native_species)
+  return(native_num)
 
 }
 
@@ -54,7 +74,12 @@ native_species_richness <- function(x) {
 
 #' Calculate Mean C
 #'
-#' @param x A data frame containing a list of plant species. This data frame must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#'`total_mean_c()` calculates the mean conservation coefficient for all species in
+#'the site assessment.
+#'
+#' @param x A data frame containing a list of plant species. This data frame
+#' must have one of the following columns: `scientific_name`, `acronym`, or
+#' `common_name`.
 #'
 #'
 #' @return A non-negative integer
@@ -67,7 +92,15 @@ native_species_richness <- function(x) {
 total_mean_c <- function(x) {
 
   #join scores from michigan fqai to user's assessment
-  user_list_with_scores <- dplyr::left_join(x, michigan_2014_fqai)
+  user_list_with_scores <- suppressMessages(
+    dplyr::left_join(x, michigan_2014_fqai))
+
+  #send warning to user if site assessment contains plant not in FQAI database
+  if( any(is.na(user_list_with_scores$c)) )
+    message("Species not listed in database, it will be given a C value of 0")
+
+  #make it so non-matching plants have a c value of 0
+  user_list_with_scores$c[is.na(user_list_with_scores$c)] <- 0
 
   #calculate mean C
   mean_c <- mean(user_list_with_scores$c)
@@ -81,7 +114,12 @@ total_mean_c <- function(x) {
 
 #' Calculate Native Mean C
 #'
-#' @param x A data frame containing a list of plant species. This data frame must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#' `native_mean_c()` calculates the mean conservation coefficient for all native
+#' species in the site assessment.
+#'
+#' @param x A data frame containing a list of plant species. This data frame
+#' must have one of the following columns: `scientific_name`, `acronym`, or
+#' `common_name`.
 #'
 #' @return A non-negative integer
 #' @export
@@ -93,11 +131,19 @@ total_mean_c <- function(x) {
 native_mean_c <- function(x) {
 
   #join scores from michigan fqai to user's assessment
-  user_list_with_scores <- dplyr::left_join(x, michigan_2014_fqai) %>%
+  user_list_with_scores <- suppressMessages(
+    dplyr::left_join(x, michigan_2014_fqai))
+
+  #send warning to user if site assessment contains plant not in FQAI database
+  if( any(is.na(user_list_with_scores$c)) )
+    message("Species not listed in database, it will be given a C value of 0")
+
+  #select only native species
+  native_species <- user_list_with_scores %>%
     dplyr::filter(native == "native")
 
   #calculate mean C
-  mean_c <- mean(user_list_with_scores$c)
+  mean_c <- mean(native_species$c)
 
   #print
   return(mean_c)
@@ -108,7 +154,13 @@ native_mean_c <- function(x) {
 
 #' Calculate Total FQI
 #'
-#' @param x A data frame containing a list of plant species. This data frame must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#' `total_FQI()` calculates the Floristic Quality Index (FQI) for the site using
+#' all species listed. FQI is found by multiplying the total mean C by the square
+#' root of the total species richness.
+#'
+#' @param x A data frame containing a list of plant species. This data frame
+#' must have one of the following columns: `scientific_name`, `acronym`, or
+#' `common_name`.
 #'
 #' @return A non-negative integer
 #' @export
@@ -119,10 +171,8 @@ native_mean_c <- function(x) {
 
 total_FQI <- function(x) {
 
-  #join scores from michigan fqai to user's assessment
-  user_list_with_scores <- dplyr::left_join(x, michigan_2014_fqai)
 
-  #calculate mean C
+  #calculate total fqi
   fqi <- total_mean_c(x) * sqrt(total_species_richness(x))
 
   #print
@@ -134,7 +184,13 @@ total_FQI <- function(x) {
 
 #' Calculate Native FQI
 #'
-#' @param x A data frame containing a list of plant species. This data frame must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#' `native_FQI()` calculates the Floristic Quality Index (FQI) for the site using
+#' only native species. Native FQI is found by multiplying the native mean C by
+#' the square root of the native species richness.
+#'
+#' @param x A data frame containing a list of plant species. This data frame
+#' must have one of the following columns: `scientific_name`, `acronym`, or
+#' `common_name`.
 #'
 #' @return A non-negative integer
 #' @export
@@ -145,11 +201,7 @@ total_FQI <- function(x) {
 
 native_FQI <- function(x) {
 
-  #join scores from michigan fqai to user's assessment
-  user_list_with_scores <- dplyr::left_join(x, michigan_2014_fqai) %>%
-    dplyr::filter(native == "native")
-
-  #calculate mean C
+  #calculate native fqi
   fqi <- native_mean_c(x) * sqrt(native_species_richness(x))
 
   #print
@@ -161,7 +213,14 @@ native_FQI <- function(x) {
 
 #' Calculate Adjusted FQI
 #'
-#' @param x A data frame containing a list of plant species. This data frame must have one of the following columns: `scientific_name`, `acronym`, or `common_name`.
+#' `adjusted_FQI()` calculates the Adjusted Floristic Quality Index (FQI) for the
+#' site using all species. Adjusted FQI is found by multiplying 100 by the native
+#' mean C divided by ten and then multiplied by the square root of native species
+#' richness divided by the square root of total species richness.
+#'
+#' @param x A data frame containing a list of plant species. This data frame
+#' must have one of the following columns: `scientific_name`, `acronym`, or
+#' `common_name`.
 #'
 #' @return A non-negative integer
 #' @export
@@ -172,14 +231,57 @@ native_FQI <- function(x) {
 
 adjusted_FQI <- function(x) {
 
-  #join scores from michigan fqai to user's assessment
-  user_list_with_scores <- dplyr::left_join(x, michigan_2014_fqai)
-
-  #calculate mean C
+  #calculate adjusted fqi
   fqi <- 100 * (native_mean_c(x)/10) *
     (sqrt(native_species_richness(x))/sqrt(total_species_richness(x)))
 
   #print
   return(fqi)
+
+}
+
+#-------------------------------------------------------------------------------
+
+#' Calculate All FQA Metrics
+#'
+#' `all_metrics()` calculates and prints Total Species Richness, Native Species Richness,
+#' Mean C, Native Mean C, Total FQI, Native FQI, and Adjusted FQI.
+#'
+#' @param x A data frame containing a list of plant species. This data frame
+#' must have one of the following columns: `scientific_name`, `acronym`, or
+#' `common_name`.
+#'
+#' @return A data frame
+#' @export
+#'
+#' @examples
+#' plant_list <- crooked_island_site_assessment
+#' adjusted_FQI(x = plant_list)
+
+all_metrics <- function(x) {
+
+#create list of all metrics that will be included in the output
+metrics <- c("Total Species Richness",
+            "Native Species Richness",
+            "Mean C",
+            "Native Mean C",
+            "Total FQI",
+            "Native FQI",
+            "Adjusted FQI")
+
+#create list of values
+values <- c(total_species_richness(x),
+           native_species_richness(x),
+           total_mean_c(x),
+           native_mean_c(x),
+           total_FQI(x),
+           native_FQI(x),
+           adjusted_FQI(x))
+
+#bind metrics and values into data frame
+report <- data.frame(metrics, values)
+
+#return the data frame
+return(report)
 
 }
